403899f41d20feeedd9d35345159a1e9f6b331f8
Darren <darren.victoriano@gmail.com>
2020-07-08 18:14:17 -0700

    working promise shell

--- Middleware/auditApps.js
+++ Middleware/auditApps.js
@@ -1,21 +1,40 @@
 const shell = require('shelljs');
 
-function sendCommand(cmd, cb) {
-    let child = shell.exec(cmd, { async: true });
+const sendCommand = (cmd) => {
+    return new Promise((resolve, reject) => {
+        let child = shell.exec(cmd, { async: true });
 
-    child.stdout.on('data', function (data) {
-        cb(data);
+        child.stdout.on('data', function (data) {
+            // check if data does not contain data
+            if (!data) reject(new Error("Data Undefined"));
+
+            // resolve if it does
+            resolve(data);
+        })
     });
 }
 
-function adbDevices(cb) {
-    let child = shell.exec("adb devices", { async: true });
+const adbDevices = () => {
+    return new Promise((resolve, reject) => {
+        let child = shell.exec("adb devices", { async: true });
 
-    child.stdout.on('data', function (data) {
-        cb(data);
+        child.stdout.on('data', function (data) {
+            // check if data does not contain data
+            if (!data) reject(new Error("Data Undefined"));
+
+            // resolve if it does
+            resolve(data);
+        });
+
     });
+
+
+
 }
 
-sendCommand("adb devices", (data) => {
-    console.log("asd" + data);
-});
+const x = async () => {
+    asd = await adbDevices();
+    console.log("asd");
+}
+
+x();
